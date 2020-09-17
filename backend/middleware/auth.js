545ef90d6d2e1fc6ec61df954b4cd30da7bb0391
Noor <engrnoorahmed407@gmail.com>
2020-09-17 09:47:39 -0700

    JWT

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,6 @@ function auth(req, res,next){
     const token = req.header('x-auth-token');
 
     //Check For Token
-
     if(!token) res.status(401).json({"msg" : "No Authorization Denied"});
 
     try{
